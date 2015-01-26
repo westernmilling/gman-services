ff04f6ceb43845848487ec43f1c9b1a67eedda2c
MichaelAChrisco <michaelachrisco@gmail.com>
2015-01-26 10:01:12 -0800

    Renamed production_url and production_user to deployment_url and deployment_user.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,20 @@
-# config valid only for current version of Capistrano
 lock '3.3.5'
 
 set :application, Figaro.env.application
 set :repo_url, Figaro.env.repo_url
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 set :branch, 'master'
 
 set :user, Figaro.env.production_user
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/#{fetch(:user)}/www"
 
-# Default value for :scm is :git
-# set :scm, :git
 set :linked_files,
     %w{config/application.yml config/database.yml config/shards.yml}
-# Default value for :format is :pretty
-# set :format, :pretty
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
 set :linked_dirs,
     %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
 set :keep_releases, 5
 
 set :normalize_asset_timestamps,
@@ -44,8 +24,6 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), :in => :sequence, :wait => 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
@@ -53,10 +31,6 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), :in => :groups, :limit => 3, :wait => 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
 end
