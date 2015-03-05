d92b24c55f84df802437cb96f46705cd2e4288dc
MichaelAChrisco <michaelachrisco@gmail.com>
2015-03-05 12:48:16 -0800

    Deploy's no longer are dependant on Rails Environment.

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 load_params = YAML::load_file('config/application.yml')
-server_params = load_params[Rails.env.to_s].symbolize_keys
+server_params = load_params['development'].symbolize_keys
 
 server server_params[:deployment_url],
        :user => server_params[:deployment_user],
