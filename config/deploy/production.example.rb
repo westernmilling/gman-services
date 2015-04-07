f4484aace84e9a516d3209bca66464a1c19873d5
michael <michaelachrisco@gmail.com>
2015-04-07 08:12:41 -0700

    Removed rvm and standardized deploy.

---
+++ config/deploy/production.example.rb
@@ -0,0 +1,9 @@
+server 'ip_to_change',
+       :user => 'user_to_change',
+       :port => 9999,
+       :roles => %w{app db web}
+
+server 'ip_to_change',
+       :user => 'user_to_change',
+       :port => 9999,
+       :roles => %w{web}
--- vm/user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#Needs to be manually done.
-sudo adduser gman
-sudo adduser gman sudo
- #add config/application.yml config/database.yml config/shards.yml config/secrets.yml
-
