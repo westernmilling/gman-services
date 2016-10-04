3745f9266cf276205885ee428e85f5a2a275b1a3
Eric Cranston <cracell@gmail.com>
2016-10-04 10:19:02 -0700

    Add program name and simplify logger.level setup ID:512

--- config/application.rb
+++ config/application.rb
@@ -36,13 +36,13 @@ Rails.application.configure do
   logger = if Figaro.env.REMOTE_LOG_HOST.present? &&
               Figaro.env.REMOTE_LOG_PORT.present?
              RemoteSyslogLogger.new(Figaro.env.REMOTE_LOG_HOST,
-                                    Figaro.env.REMOTE_LOG_PORT)
+                                    Figaro.env.REMOTE_LOG_PORT,
+                                    program: "gman-services-#{RAILS_ENV}")
            else
              Logger.new(STDOUT)
            end
 
   config.log_formatter = ::Logger::Formatter.new
   config.logger = logger
-  config.logger.level = Logger.const_get('INFO')
-  config.log_level = (Figaro.env.REMOTE_LOG_LEVEL || 'info').to_sym
+  config.logger.level = (Figaro.env.REMOTE_LOG_LEVEL || 'info').to_sym
 end
