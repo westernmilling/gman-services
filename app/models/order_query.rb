70159b8b7581e6ae825aab6984a9ac45f98459b5
Eric Cranston <cracell@gmail.com>
2016-10-12 11:13:34 -0700

    Change validations to generate an exception when parameters are incorrect ID:575

--- app/models/order_query.rb
+++ app/models/order_query.rb
@@ -3,10 +3,11 @@ class OrderQuery
 
   attr_accessor :warehouse_id, :order_number
 
-  validates :warehouse_id, length: { is: 2 }
-  validates :order_number, length: { is: 6 }
+  validates! :warehouse_id, length: { is: 2 }
+  validates! :order_number, length: { is: 6 }
 
   def to_query
+    valid? # This will trigger an error if parameters are invalid
     # I may be defeating the point of sanitizing here but the sanizitize method
     # wraps the query in quotes so I remove them after because Relativity
     # doesn't seem to be capable of handling that
