8ef306aaa00244caf10aff17bf50c760a320708c
Joseph Bridgwater-Rowe <joe@westernmilling.com>
2017-08-14 17:01:41 -0700

    Add Fob Location to contract API ID:3076
    Add the Fob Location value to the contract API response so that
    clients know if a contract is Delivered or FOB.

--- spec/factories/contracts.rb
+++ spec/factories/contracts.rb
@@ -10,6 +10,7 @@ FactoryGirl.define do
     CONT_ContractType { %w{P S}.sample }
     CONT_Quantity { [50_000, 100_000].sample }
     CONT_DeliveredBushels { Faker::Number.between(0, self.CONT_Quantity) }
+    CONT_FobLocationDescription1 { %w(D F).sample }
     CONT_FreightAdjustment { [0, 5, 6, 7].sample }
     CONT_FromDate { Date.current }
     CONT_ToDate { Date.current + 1.year }
