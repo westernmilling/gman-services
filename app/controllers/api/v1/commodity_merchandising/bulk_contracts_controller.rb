84be04f4e50b3bf5144cd5f5f62e74be1751df68
Eric Cranston <cracell@gmail.com>
2018-08-27 11:15:53 -0700

    Include Commodity conversion factor with bulk contracts (#110)
    Include commodity conversion factor so we can convert the 
    quantity into pounds.
    
    Remove unused columns to speed things up.
    
    needed-by #107

--- app/controllers/api/v1/commodity_merchandising/bulk_contracts_controller.rb
+++ app/controllers/api/v1/commodity_merchandising/bulk_contracts_controller.rb
@@ -20,16 +20,52 @@ module Api
           "select #{column_names.join(', ')} from Contract " \
           'INNER JOIN InvPickUpOrders on ' \
           'InvPickUpOrders.ContractId = Contract.Inv_ContractId ' \
+          'INNER JOIN Commodity on ' \
+          'Commodity.CommodityId = Contract.CommodityId ' \
           'and Contract.LocationId = InvPickUpOrders.ContractLocationId'
         end
+        # Worked with commodity id. Took 1:17
 
         def column_names
-          column_name_generator(Contract) + column_name_generator(PickUpOrder)
+          contract_column_names +
+            pick_up_order_column_names +
+            commodity_column_names
         end
 
-        def column_name_generator(model_class)
-          model_class.column_names.map do |name|
-            "#{model_class.table_name}.#{name}"
+        def commodity_column_names
+          column_name_generator(Commodity, ['COMM_ConversionFactor'])
+        end
+
+        def contract_column_names
+          columns = %w(
+            CustomerId
+            CONT_ContractDate
+            CONT_ContractType
+            CONT_FobLocationDescription1
+            CONT_Price
+            CONT_Quantity
+            CONT_FromDate
+            CONT_ToDate
+          )
+          column_name_generator(Contract, columns)
+        end
+
+        def pick_up_order_column_names
+          columns = %w(
+            ItemId
+            Origin
+            OriginState
+            PickupType
+            ReleaseLoadNumber
+            ReleasePrefix
+            Status
+          )
+          column_name_generator(PickUpOrder, columns)
+        end
+
+        def column_name_generator(model_class, columns)
+          columns.map do |name|
+            [model_class.table_name, name].join('.')
           end
         end
       end
